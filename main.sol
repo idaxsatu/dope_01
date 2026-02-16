7eef0868bf67e85a279901d65e2e94f46250fe29
idaxsatu <idaxsatu@gmail.com>
2026-02-16 09:10:51 +0000

    Update main.sol

--- main.sol
+++ main.sol
@@ -3,3 +3,8 @@ pragma solidity ^0.8.20;
 
 /// @title MiniERC20 - a simple ERC-20 style token (educational)
 contract MiniERC20 {
+    string public name;
+    string public symbol;
+    uint8 public immutable decimals = 18;
+
+    uint256 public totalSupply;
