956f448f45ec5bab203d86ccd8a9db31ebd6db55
idaxsatu <idaxsatu@gmail.com>
2026-02-16 09:11:10 +0000

    Update main.sol

--- main.sol
+++ main.sol
@@ -8,3 +8,8 @@ contract MiniERC20 {
     uint8 public immutable decimals = 18;
 
     uint256 public totalSupply;
+
+    mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
