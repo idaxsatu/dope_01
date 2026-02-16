6275cf580400d5f29fe715024e5ae836bddc1a4e
idaxsatu <idaxsatu@gmail.com>
2026-02-16 09:12:19 +0000

    Update main.sol

--- main.sol
+++ main.sol
@@ -28,3 +28,8 @@ contract MiniERC20 {
         balanceOf[msg.sender] = mintAmount;
 
         emit Transfer(address(0), msg.sender, mintAmount);
+    }
+
+    function transfer(address to, uint256 value) external returns (bool) {
+        _transfer(msg.sender, to, value);
+        return true;
